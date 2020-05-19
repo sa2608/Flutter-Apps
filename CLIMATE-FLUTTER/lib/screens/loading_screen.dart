f2b9cc2227614360df740777bd30587dfb31046b
sanikumar sahani <sanikumarsahani@gmail.com>
2020-05-19 07:25:21 +0530

    weather checking app

---
+++ CLIMATE-FLUTTER/lib/screens/loading_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'location_screen.dart';
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import '../services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+ 
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+    print('this line of code is triggered');
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+
+    NetworkHelper networkHelper = NetworkHelper(
+        //fetch the weather data for the location
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper
+        .getData(); //object created to used in location screen
+    //pass tge data to the location screen
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
