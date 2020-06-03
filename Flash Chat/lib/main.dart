4a7bb42a976d0d875a14df19f29cb5fec9272175
sanikumar sahani <sanikumarsahani@gmail.com>
2020-06-03 17:55:59 +0530

    ..

--- Flash Chat/lib/main.dart
+++ Flash Chat/lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -15,7 +19,14 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      //screen  whent the apps start
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
