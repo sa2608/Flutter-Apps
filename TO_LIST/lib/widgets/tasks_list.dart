7ca975aad42f766ce40ed342b9a7cda72cc4863c
sanikumar sahani <sanikumarsahani@gmail.com>
2020-07-06 12:48:37 +0530

    main screen, icons added, floatingbutton added, 2 containers added

---
+++ TO_LIST/lib/widgets/tasks_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/tasks_tile.dart';
+
+
+class TasksList extends StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return ListView(
+     children: [
+       TaskTile(),
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+       TaskTile(), 
+     ],   
+    );
+  }
+}
