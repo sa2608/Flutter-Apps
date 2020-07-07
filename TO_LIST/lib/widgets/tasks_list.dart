ddb60b859feeb44f73a5c76d5b00735ab27a2b8c
sanikumar sahani <sanikumarsahani@gmail.com>
2020-07-07 10:14:41 +0530

    tasks

--- TO_LIST/lib/widgets/tasks_list.dart
+++ TO_LIST/lib/widgets/tasks_list.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/tasks_tile.dart';
+import 'package:todo_app/models/tasks_screen.dart';
 
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
 
-class TasksList extends StatelessWidget {
+class _TasksListState extends State<TasksList> {
+
+    List<Task> tasks = [
+      Task(name: 'Buy milk'),
+      Task(name: 'Buy dooed'),
+      Task(name: 'Buy toothbrush'),
+    ];
     @override
   Widget build(BuildContext context) {
     return ListView(
-     children: [
-       TaskTile(),
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
-       TaskTile(), 
+     children: [ 
+       TaskTile(taskTitle: tasks[0].name, isChecked: tasks[0].isDone,), 
+       TaskTile(taskTitle: tasks[1].name, isChecked: tasks[1].isDone,), 
+       TaskTile(taskTitle: tasks[2].name, isChecked: tasks[2].isDone,), 
      ],   
     );
   }
--- TO_LIST/lib/widgets/tasks_tile.dart
+++ TO_LIST/lib/widgets/tasks_tile.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
-
-
 class TaskTile extends StatelessWidget {
-   @override
+  final String taskTitle;
+  final bool isChecked;
+  TaskTile({this.isChecked, this.taskTitle});
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("hello there"),
-      trailing: Checkbox(value: false,
-      onChanged: null,),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        // onChanged: toggleCheckboxState,
+      ),
     );
   }
 }
+
+// (bool checkboxState) {
+//             setState(() {
+//               isChecked = checkboxState;
+//             });
+//           }
